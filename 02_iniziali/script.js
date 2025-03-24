f391adca9c0489f15a02df6a9193936928a678f0
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-24 16:43:28 +0100

    [UPD] secondo

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function inizialiNomi(names){
+    let iniziali = [];
+    for (let i = 0; i < names.length; i++){
+        iniziali.push(names[i].charAt(0));
+    }
+    return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(inizialiNomi(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
