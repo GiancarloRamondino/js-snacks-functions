c3dbaa6e7d3fac0a89b7303390ca5e0e721f1315
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-24 16:27:58 +0100

    [UPD] primo

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+
 
 
 // Dichiara la funzione qui.
-
+const userName = prompt('inserisci il tuo nome');
+let saluto = 'Ciao';
+let salutoCompleto = saluto +' '+ userName;
 
 // Invoca la funzione qui e stampa il risultato in console
-
+function salutoUtente(userName){
+    return salutoCompleto= saluto +' '+ userName;
+}
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(salutoUtente(userName));
