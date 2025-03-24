b254a195097fd0cbf2988bc222f82ce5d7a3208b
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-24 17:01:55 +0100

    [UPD] quarto

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+let vocali = ['a','e','i','o','u'];
+function contaVocali(word){
+    let conta=0;
+    for (let i=0; i<word.length; i++){
+        if(vocali.includes(word[i]))
+            conta++;
+    }
+    return conta;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(contaVocali(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
