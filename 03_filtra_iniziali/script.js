e7e8b4da2b3b234f1ba204edef720343dd4b8e9c
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-24 16:54:27 +0100

    [UPD] 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+let lettera = prompt('inserisci una lettera');
+function filtraIniziali(names, lettera){
+    let iniziali = [];
+    for (let i=0; i < names.length; i++){
+        if (names[i][0].toUpperCase() === lettera.toUpperCase()){
+            iniziali.push(names[i]);
+        }
+    }
+    return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filtraIniziali(names,lettera));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
